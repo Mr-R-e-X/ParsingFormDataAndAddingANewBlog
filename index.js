2fd966a924ee5047985622fa835c2319b3f07ef3
Mr-R-e-X <souvikhazra151@gmail.com>
2024-07-16 10:54:15 +0530

    assignment completed

---
+++ index.js
@@ -0,0 +1,23 @@
+// Import the necessary modules here
+import express from "express";
+import path from "path";
+import expressEjsLayouts from "express-ejs-layouts";
+import {
+  renderBlogs,
+  renderBlogForm,
+  addBlog,
+} from "./src/controllers/blog.controller.js";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
+app.use(expressEjsLayouts);
+
+// Write your code here
+app.get("/", renderBlogs);
+app.get("/createblog", renderBlogForm);
+app.post("/addblog", addBlog);
+
+export default app;
